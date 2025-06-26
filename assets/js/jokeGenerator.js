7d04209de3a4b583b6a55d17c79237bc4cd1caa7
grascon <gilberto.rascon.serventi@gmail.com>
2025-06-26 11:46:14 -0700

    Progress update 1
    Added all HTML elements and JS functions

---
+++ assets/js/jokeGenerator.js
@@ -0,0 +1,32 @@
+window.addEventListener('load', () => {
+
+    const jokeButton = document.getElementById('joke_button');
+    const jokeText = document.getElementById('joke_text');
+
+    const updateJoke = async () => {
+        const newJoke = await jokeCall();
+        jokeText.innerText = newJoke;
+        jokeText.classList.add('display');
+    }
+
+    const jokeCall = async () => {
+
+        try {
+            const url = "https://icanhazdadjoke.com/";
+            const config = { headers: { Accept: 'application/json' } };
+            const res = await fetch(url, config);
+            const data = await res.json();
+            console.log(data);
+            const joke = data.joke;
+            return joke;
+
+        } catch (error) {
+            console.log(`Error: ${error}`);
+            return error;
+        }
+    }
+
+    jokeButton.addEventListener('click', () => {
+        updateJoke();
+    })
+})
